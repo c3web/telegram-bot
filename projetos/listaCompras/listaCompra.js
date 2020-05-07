e1766408a29f4aa7f892dc24fb46712d96118f3b
Cesar Augusto Ferreira <c3web@hotmail.com>
2020-05-07 11:39:22 -0300

    Projeto Lista de Compras

---
+++ projetos/listaCompras/listaCompra.js
@@ -0,0 +1,33 @@
+const env = require('../../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const bot = new Telegraf(env.token)
+
+let lista = []
+
+const gerarBotoes = () => Extra.markup(
+    Markup.inlineKeyboard(
+        lista.map(item => Markup.callbackButton(item, `delete ${item}`)),
+        { columns: 3 }
+    )
+)
+
+bot.start(async ctx => {
+    const name = ctx.update.message.from.first_name
+    await ctx.reply(`Seja bem-vindo, ${name}`)
+    await ctx.reply('Escreva os itens que você deseja adicionar...')
+})
+
+bot.on('text', ctx => {
+    const item = ctx.update.message.text
+    lista.push(item)
+    ctx.reply(`${item} adicionado!`, gerarBotoes())
+})
+
+bot.action(/delete (.+)/, ctx => {
+    lista = lista.filter(item => item !== ctx.match[1])
+    ctx.reply(`${ctx.match[1]} deletado`, gerarBotoes())
+})
+
+bot.startPolling()
